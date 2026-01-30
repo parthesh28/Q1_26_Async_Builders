90178ac6a89d1ec922382a25c1260f4ec510a87b
Parthesh28 <partheshpurohit28@gmail.com>
2026-01-30 13:14:47 +0530

    finished stake and unstake

--- anchor-nft-staking-starter-q4-25/programs/anchor-nft-staking-q4-25/src/instructions/stake.rs
+++ anchor-nft-staking-starter-q4-25/programs/anchor-nft-staking-q4-25/src/instructions/stake.rs
@@ -16,24 +16,69 @@ pub struct Stake<'info> {
     pub user : Signer<'info>,
     #[account(
         mut,
-        constraint = asset.owner == &CORE_PROGRAM_ID @ StakeError::InvalidAssetOwner,
+        constraint = asset.owner == &CORE_PROGRAM_ID,
         constraint = !asset.data_is_empty() @ StakeError::AssetNotInitialized
     )]
+    /// Checked: Verified by mpl-core
     pub asset : UncheckedAccount<'info>,
        #[account(
         mut,
-        constraint = collection.owner == &CORE_PROGRAM_ID @ StakeError::InvalidAssetOwner,
+        constraint = collection.owner == &CORE_PROGRAM_ID,
         constraint = !collection.data_is_empty() @ StakeError::AssetNotInitialized
     )]
+    /// Checked: Verified by mpl-core
     pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        init, 
+        payer = user,
+        seeds = [b"stake", stake_config.key().as_ref(), asset.key().as_ref()],
+        bump,
+        space = StakeAccount::DISCRIMINATOR.len() + StakeAccount::INIT_SPACE
+    )]
     pub stake_account : Account<'info, StakeAccount>,
+
+    #[account(
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = stake_config.bump
+    )]
     pub stake_config : Account<'info, StakeConfig>,
+
+    #[account(
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump
+    )]
     pub user_account : Account<'info, UserAccount>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    pub core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
 
 }
 
-// impl<'info> Stake<'info> {
-//     pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
-//TODO
-//     }
-// }
+impl<'info> Stake<'info> {
+    pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
+
+        require!(self.user_account.amount_staked < self.stake_config.max_stake, StakeError::MaxStakeReached);
+
+        AddPluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(None)
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: true}))
+            .init_authority(PluginAuthority::Address { address: self.stake_account.key() })
+            .invoke()?;
+
+        self.stake_account.set_inner(StakeAccount { 
+            owner: self.user.key(),
+            mint: self.asset.key(), 
+            staked_at: Clock::get()?.unix_timestamp, 
+            bump: bumps.stake_account 
+        });
+
+        Ok(())
+
+    }
+}
